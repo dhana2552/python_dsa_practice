580ee1c618604bdd1b396c11b58b925c5afb25b5
dhana2552 <dhana2552@gmail.com>
2025-12-16 09:28:50 +0530

    get method added

---
+++ DoublyLinkedList/dll_get.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length-1, index, -1):
+                temp = temp.prev
+        return temp
+
+
+my_doubly_linked_list = DoublyLinkedList(0)
+my_doubly_linked_list.append(5)
+my_doubly_linked_list.append(8)
+my_doubly_linked_list.append(2)
+
+print('Get node from first half of DLL:')
+print(my_doubly_linked_list.get(1).value)
+
+print('\nGet node from second half of DLL:')
+print(my_doubly_linked_list.get(2).value)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Get node from first half of DLL:
+    1
+
+    Get node from second half of DLL:
+    2
+
+"""
