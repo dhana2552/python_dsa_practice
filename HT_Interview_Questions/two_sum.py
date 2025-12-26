45f6d5734596e9bbbf87c9ef6a43b31c36bd36c1
dhana2552 <dhana2552@gmail.com>
2025-12-26 15:37:46 +0530

    two_sum problem solved

---
+++ HT_Interview_Questions/two_sum.py
@@ -0,0 +1,34 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+print(two_sum([1, 3, 5, 7, 9], 10))  
+print ( two_sum([1, 2, 3, 4, 5], 10) )
+print ( two_sum([1, 2, 3, 4, 5], 7) )
+print ( two_sum([1, 2, 3, 4, 5], 3) )
+print ( two_sum([], 0) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
+
+
