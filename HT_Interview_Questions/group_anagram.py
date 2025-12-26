91ff9a5450ffe14d33579f1a2318df1770e1b7ac
dhana2552 <dhana2552@gmail.com>
2025-12-26 15:28:32 +0530

    anagram group

---
+++ HT_Interview_Questions/group_anagram.py
@@ -0,0 +1,34 @@
+def group_anagrams(list1):
+    anagram_group = {}
+    for string in list1:
+        word = ''.join(sorted(string))
+        if word in anagram_group:
+            anagram_group[word].append(string)
+        else:
+            anagram_group[word] = [string]
+    return list(anagram_group.values())
+
+print("1st set:")
+print( group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) )
+
+print("\n2nd set:")
+print( group_anagrams(["abc", "cba", "bac", "foo", "bar"]) )
+
+print("\n3rd set:")
+print( group_anagrams(["listen", "silent", "triangle", "integral", "garden", "ranged"]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1st set:
+    [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+
+    2nd set:
+    [['abc', 'cba', 'bac'], ['foo'], ['bar']]
+
+    3rd set:
+    [['listen', 'silent'], ['triangle', 'integral'], ['garden', 'ranged']]
+
+"""
