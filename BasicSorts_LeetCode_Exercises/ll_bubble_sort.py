f305982dfae3d73b00f83e685ec4e6ab16535154
dhana2552 <dhana2552@gmail.com>
2025-12-31 11:45:43 +0530

    Bubble sort in linked list code added

---
+++ BasicSorts_LeetCode_Exercises/ll_bubble_sort.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def bubble_sort(self):
+        if self.length < 2 or self.head is None:
+            return
+        for i in range(self.length, 1, -1):
+            temp = self.head
+            for j in range(1, i):
+                if temp.value > temp.next.value:
+                    temp.value, temp.next.value = temp.next.value, temp.value #swap only values
+                temp = temp.next #move to the next pointer
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.bubble_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
